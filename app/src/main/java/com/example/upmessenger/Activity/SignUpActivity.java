1b309ee135778c79d9a78279397901bb27d21f1f
CHetan7-web <56154872+CHetan7-web@users.noreply.github.com>
2021-09-07 15:58:16 +0530

    Authentication Done

--- app/src/main/java/com/example/upmessenger/Activity/SignUpActivity.java
+++ app/src/main/java/com/example/upmessenger/Activity/SignUpActivity.java
@@ -17,6 +17,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.upmessenger.Models.UpUsers;
 import com.example.upmessenger.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -26,6 +32,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -34,6 +41,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
+import java.util.Arrays;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -43,11 +51,13 @@ public class SignUpActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     GoogleSignInClient mGoogleSignInClient ;
-// ...
 
+    LoginButton btnFacebook;
     Button btnSignUp,btnGoogle;
     EditText etName,etEmail ,etPassword;
     TextView tvSignIn;
+
+    CallbackManager callbackManager;
 
     ProgressDialog proDialog;
     @Override
@@ -55,6 +65,8 @@ public class SignUpActivity extends AppCompatActivity {
 
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("Users");
+
+        callbackManager = CallbackManager.Factory.create();
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
@@ -70,6 +82,8 @@ public class SignUpActivity extends AppCompatActivity {
 
         btnSignUp = findViewById(R.id.btnSignUp);
         btnGoogle = findViewById(R.id.btnGoogle);
+        btnFacebook = (LoginButton) findViewById(R.id.login_button);
+        btnFacebook.setReadPermissions(Arrays.asList("email"));
 
         proDialog = new ProgressDialog(SignUpActivity.this);
         proDialog.setTitle("Signing Up ..");
@@ -128,6 +142,28 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
 
+        // Callback registration
+        btnFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                Log.d("FACEBOOK_USER",""+loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+
     }
 
     private void signInWithGoogle() {
@@ -135,9 +171,52 @@ public class SignUpActivity extends AppCompatActivity {
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d("TAG", "handleFacebookAccessToken:" + token);
+        proDialog.show();
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            UpUsers myuser = new UpUsers();
+                            myuser.setEmail(user.getEmail());
+                            myuser.setUserId(user.getUid());
+                            myuser.setName(user.getDisplayName());
+                            myuser.setProfilePic(user.getPhotoUrl().toString());
+
+                            proDialog.dismiss();
+
+                            Toast.makeText(getApplicationContext(),"Current id :"+myuser.getUserId(),Toast.LENGTH_LONG).show();
+
+                            myRef.child(myuser.getUserId()).setValue(myuser);
+
+                            Intent intent = new Intent(SignUpActivity.this,MainActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(getApplicationContext(),"SUCCess",Toast.LENGTH_SHORT).show();
+
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
