afae7c3427d5b1234285c8e1eaa81994ebdd3be1
CHetan7-web <56154872+CHetan7-web@users.noreply.github.com>
2021-09-07 18:40:46 +0530

    Squashed commit of the following:
    commit 1b309ee135778c79d9a78279397901bb27d21f1f
    Author: CHetan7-web <56154872+CHetan7-web@users.noreply.github.com>
    Date:   Tue Sep 7 15:58:16 2021 +0530
    
        Authentication Done
    
    commit 8e9ffec8506a7561e69b07c9964239c9870d9bf9
    Author: CHetan7-web <56154872+CHetan7-web@users.noreply.github.com>
    Date:   Sun Sep 5 14:27:27 2021 +0530
    
        Facebook Authentication Added
    
    commit 65a4d3865f5f7805926e0a6a9bc820f3d39e2afa
    Author: CHetan7-web <56154872+CHetan7-web@users.noreply.github.com>
    Date:   Sun Sep 5 10:07:50 2021 +0530
    
        UI Updated
    
    commit 594cf7e4d4a308e128d20ec873b423cc820b2830
    Author: CHetan7-web <56154872+CHetan7-web@users.noreply.github.com>
    Date:   Sun Sep 5 09:26:15 2021 +0530
    
        Updated Buttons position
    
    commit f1e995870cfe1cb34e8b4543f6f3a10eee274310
    Author: CHetan7-web <56154872+CHetan7-web@users.noreply.github.com>
    Date:   Sun Sep 5 09:16:19 2021 +0530
    
        Authentication Updated

--- app/src/main/java/com/example/upmessenger/Activity/SignInActivity.java
+++ app/src/main/java/com/example/upmessenger/Activity/SignInActivity.java
@@ -15,7 +15,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.upmessenger.Models.UpUsers;
 import com.example.upmessenger.R;
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+//import com.facbook.
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -25,6 +34,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -33,9 +43,13 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SignInActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 63;
+    private static final String EMAIL = "chetanurkude111@gmail.com" ;
 
     private FirebaseDatabase db;
     private DatabaseReference myRef;
@@ -43,6 +57,7 @@ public class SignInActivity extends AppCompatActivity {
     GoogleSignInClient mGoogleSignInClient ;
 
     Button btnSignIn,btnGoogle;
+    LoginButton loginButton;
     EditText etName,etEmail ,etPassword;
     TextView tvSignUp;
 
@@ -52,6 +67,8 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_sign_in);
 
         mAuth = FirebaseAuth.getInstance();
@@ -70,6 +87,35 @@ public class SignInActivity extends AppCompatActivity {
         btnSignIn = findViewById(R.id.btnSignIn);
         btnGoogle = findViewById(R.id.btnGoogle);
 
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("email"));
+
+        proDialog = new ProgressDialog(SignInActivity.this);
+        proDialog.setTitle("Signing In ..");
+        proDialog.setMessage("You are Signing in in some moments .. . ");
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                Log.d("FACEBOOK_USER",""+loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
         tvSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -78,9 +124,7 @@ public class SignInActivity extends AppCompatActivity {
             }
         });
 
-        proDialog = new ProgressDialog(SignInActivity.this);
-        proDialog.setTitle("Signing In ..");
-        proDialog.setMessage("You are Signing in in some moments .. . ");
+
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -88,7 +132,6 @@ public class SignInActivity extends AppCompatActivity {
                 .build();
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -131,6 +174,8 @@ public class SignInActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
@@ -177,6 +222,46 @@ public class SignInActivity extends AppCompatActivity {
                 });
     }
 
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d("TAG", "handleFacebookAccessToken:" + token);
+        proDialog.show();
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            UpUsers myuser = new UpUsers();
+                            myuser.setEmail(user.getEmail());
+                            myuser.setUserId(user.getUid());
+                            myuser.setName(user.getDisplayName());
+                            myuser.setProfilePic(user.getPhotoUrl().toString());
+
+                            proDialog.dismiss();
+
+                            Toast.makeText(getApplicationContext(),"Current id :"+myuser.getUserId(),Toast.LENGTH_LONG).show();
+
+                            myRef.child(myuser.getUserId()).setValue(myuser);
+
+                            Intent intent = new Intent(SignInActivity.this,MainActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(getApplicationContext(),"SUCCess",Toast.LENGTH_SHORT).show();
+
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+                    }
+                });
+    }
 
     @Override
     protected void onStart() {
